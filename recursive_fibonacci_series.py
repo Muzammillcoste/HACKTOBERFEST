6fd19d0c2d1c445cf08adc4c24cedf1159896405
muzammillcoste <muzammilrizvi977@gmail.com>
2022-10-05 11:35:52 +0500

    add fibonacci series generator func

---
+++ recursive_fibonacci_series.py
@@ -0,0 +1,15 @@
+def Fibonacci(n):
+    '''This function generate fibonacci series'''
+    # Check if input is less than 0 then it will print incorrect input
+    if n < 0:
+        print("Incorrect input")
+    # Check if n is 0 then it will return 0
+    elif n == 0:
+        return 0
+    # Check if n is 1,2  it will return 1
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return (Fibonacci(n-1) + Fibonacci(n-2))
+      
+print(Fibonacci(10),end=' ')
